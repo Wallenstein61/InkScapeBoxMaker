c5c51cfb317348111c2764fe2abee77e8da0f1d6
Michael Breu <Wallenstein61@@users.noreply.github.com>
2019-03-11 14:04:30 +0100

    Fixed Renaming

--- src/boxmakerN.py
+++ src/boxmakerN.py
@@ -23,7 +23,7 @@ Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 
 __version__ = "0.1" 
 
-from boxmakerLib import BoxMaker
+from boxmakerNLib import BoxMaker
 # Create effect instance and apply it.
 effect = BoxMaker()
 effect.affect()
